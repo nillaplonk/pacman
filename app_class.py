bd216140a81f4fd20f4ba4a8ab1832f54da57311
nillaplonk <nielzjahhhh@gmail.com>
2020-05-12 17:26:21 +0200

    huh

--- app_class.py
+++ app_class.py
@@ -24,8 +24,13 @@ class App:
                 self.start_events()
                 self.start_draw()
                 self.start_update()
+            elif self.state == "playing":
+                self.playing_events()
+                self.playing_draw()
+                self.playing_update()
+
             else:
-                pass
+                self.running = False
 
             self.clock.tick(FPS)
 
@@ -35,15 +40,19 @@ class App:
 
 # ---------------------wat help functies voor dingen---------------------------
 
-
     def draw_text(self, words, screen, position, size, color, font):
         font = pygame.font.SysFont(font, size)
         text = font.render(words, False, color)
         text_size = text.get_size()
+
+        # Hierdoor komt de text in het midden van het scherm
+        position[0] = position[0] - text_size[0] // 2
+        position[1] = position[1] - text_size[1] // 2
         screen.blit(text, position)
 
 
-# ------------------intro functies-----------------------------
+# ------------------INTRO  FUNCTIES -----------------------------
+
 
     def start_events(self):
         for event in pygame.event.get():
@@ -59,11 +68,33 @@ class App:
 # ---------Dit tekent de tekst het scherm en maakt gebruik van de draw_text functie---------------------
     def start_draw(self):
         self.screen.fill(BLACK)
-        self.draw_text("PRESS SPACE TO START", self.screen, (115, HEIGHT // 2), START_TEXT_SIZE,
+
+        # Text aan het begin van het spel, in het midden van het scherm. input = functie draw_text
+        self.draw_text("PRESS SPACE TO START", self.screen, [WIDTH // 2, HEIGHT // 2], START_TEXT_SIZE,
                        (170, 130, 60), START_FONT)
-        self.draw_text("PACMAN", self.screen, (150, HEIGHT // 4), START_TEXT_SIZE,
+        self.draw_text("PACMAN", self.screen, [WIDTH // 2, HEIGHT // 4.5], START_TEXT_SIZE_TITLE,
                        (40, 180, 175), START_FONT)
+        self.draw_text("@NILLAPLONK", self.screen, [WIDTH // 2, HEIGHT // 1.1], START_TEXT_SIZE_CRED,
+                       (164, 255, 55), START_FONT)
+        self.draw_text("HIGHSCORE: ", self.screen, [45, 10], START_TEXT_SIZE_CRED,
+                       (255, 255, 255), START_FONT)
+
         pygame.display.update()
         pygame.display.flip()
 
 
+# ------------------PLAYING  FUNCTIES -----------------------------
+
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def playing_update(self):
+        pass
+
+# ---------Dit tekent de tekst het scherm en maakt gebruik van de draw_text functie---------------------
+    def playing_draw(self):
+        self.screen.fill(WHITE)
+        pygame.display.flip()
--- settings.py
+++ settings.py
@@ -4,11 +4,14 @@ FPS = 60
 # Kleuren
 
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 
 # text settings
 
-START_TEXT_SIZE = 16
+START_TEXT_SIZE = 30
+START_TEXT_SIZE_TITLE = 40
+START_TEXT_SIZE_CRED = 15
 START_FONT = 'arial black'
 
 
